2f5ba0e06d24179145d66c7e8b3a8a7a0efc421a
simone <s.cimarelli@spaziocodice.com>
2025-03-04 15:50:50 +0100

    feat: init project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,66 @@
-import Image from "next/image";
+import Aurora from '@/components/bits/backgrounds/Aurora/Aurora' // Adjust the path as necessary
+import RotatingText from '@/components/bits/text-animations/RotatingText/RotatingText'
+import BlurText from '../components/bits/text-animations/BlurText/BlurText'
+import type {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: 'Simone is a dev',
+    description: 'Welcome to my personal website',
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    return (
+        <div className="min-h-screen flex flex-col justify-between pb-20 ">
+            <Aurora
+                colorStops={['#2a8e10', '#ffffff', '#FF3232']}
+                blend={0.8}
+                amplitude={1.0}
+                speed={0.5}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <div
+                className="items-center justify-items-center p-8 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+                <main className="flex flex-col gap-8 row-start-2 items-center ">
+                    <BlurText
+                        text="Simone is a dev"
+                        delay={150}
+                        animateBy="words"
+                        direction="top"
+                        className="text-7xl mb-8"
+                    />
+                    <div className="flex flex-row gap-2 items-center text-3xl">
+                        <span>Programming is</span>
+                        <RotatingText
+                            texts={['Fun', 'Passion', 'Life', 'Love', 'Art']}
+                            mainClassName="px-2 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+                            staggerFrom={'last'}
+                            initial={{y: '100%'}}
+                            animate={{y: 0}}
+                            exit={{y: '-120%'}}
+                            staggerDuration={0.025}
+                            splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+                            transition={{type: 'spring', damping: 30, stiffness: 400}}
+                            rotationInterval={2000}
+                        /></div>
+                </main>
+            </div>
+            <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+                <a
+                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+                    href="https://spaziocodice.com/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Spazio Codice
+                </a>
+                <a
+                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+                    href="https://github.com/cimasim89"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Go to my Github →
+                </a>
+            </footer>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    )
 }
